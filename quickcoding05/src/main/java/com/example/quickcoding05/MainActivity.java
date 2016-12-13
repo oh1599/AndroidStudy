f2a6691c5a901f0bf2c9b217fa0458906d97d2bc
Hansam <oh1599@kookmin.ac.kr>
2016-12-13 13:51:56 +0900

    change QuickCoding05

--- quickcoding05/src/main/java/com/example/quickcoding05/MainActivity.java
+++ quickcoding05/src/main/java/com/example/quickcoding05/MainActivity.java
@@ -3,51 +3,74 @@ package com.example.quickcoding05;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    static List<Integer> arrayInt = new ArrayList<Integer>();
-    static List<String> arrayStr = new ArrayList<String>();
-
-    static EditText get ;
-
-    protected void onCreate(Bundle savedInstanceState) {
+    ArrayList<String> arrayString=new ArrayList<String>();
+    ArrayList<Integer> arrayInteger=new ArrayList<Integer>();
+    EditText edit;
+    Button send;
+    Button result;
+    TextView number,string;
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number=(TextView)findViewById(R.id.number);
+        string=(TextView)findViewById(R.id.string);
+
+        edit=(EditText)findViewById(R.id.input);
+
+        send=(Button)findViewById(R.id.send);
+        result=(Button)findViewById(R.id.result);
+
+        send.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                if(isIntegerTrue(edit.getText().toString()))
+                {
+                    arrayInteger.add(Integer.parseInt(edit.getText().toString()));
+                }
+                else
+                {
+                    arrayString.add(edit.getText().toString());
+                }
+                edit.setText("");
+            }
+        });
+
+        result.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                number.setText(arrayInteger.toString()+" ");
+                string.setText(arrayString.toString()+" ");
+            }
+        });
     }
 
-    public void onClick01(View view) {
-        get = (EditText) findViewById(R.id.get) ;
-
-        if (isStringInteger(get.getText().toString())) {
-            arrayInt.add(Integer.parseInt(get.getText().toString()));
-        } else {
-            arrayStr.add(get.getText().toString()) ;
-        }
-
-        get.setText("");
-    }
-
-    public void onClick02(View view) {
-        TextView showInteger = (TextView) findViewById(R.id.showInteger) ;
-        TextView showStrings = (TextView) findViewById(R.id.showStrings) ;
-
-        showInteger.setText(arrayInt.toString() + " ");
-
-        showStrings.setText(arrayStr.toString() + " ");
-    }
-    public  boolean isStringInteger(String s) {
+    public boolean isIntegerTrue(String s)
+    {
         try
         {
             Integer.parseInt(s);
             return true;
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException e)
+        {
             return false;
         }
     }
+
+
+
 }
